aa572b282a8898f2a50949f1fe33f04ba1001ab1
Rendani@25 <st10334177@rcconnect.edu.za>
2025-09-16 10:19:32 +0000

    improved the previous errors

--- app/src/main/java/com/example/phambili_ma_africa/MainActivity.kt
+++ app/src/main/java/com/example/phambili_ma_africa/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.phambili_ma_africa
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // Handle Start button click -> go to LoginActivity
+        val btnStart = findViewById<Button>(R.id.startButton)
+        btnStart.setOnClickListener {
+            val intent = Intent(this,Login ::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+
